671aca159522ab18be1b65b19a4b3f7c6064d8fd
James Ward <james@calicologic.com>
2020-11-22 22:33:39 -0800

    get rid of victor, and add game mechanics

--- gravity.js
+++ gravity.js
@@ -3,21 +3,26 @@
 */
 
 // Config
-const NUM_POINTS = 5
+const NUM_POINTS = 10
 const POINT_BORDER_THICKNESS = 5
 const MAX_RADIUS = 40
 const MIN_RADIUS = 5
 const PLAYER_RADIUS = 25
 
-const POINT_DENSITY = 100
+const KEY_STRENGTH = 20
+
+const POINT_DENSITY = 15
 
 // Constants
-const KEY_W = 119
-const KEY_A = 97
-const KEY_S = 115
-const KEY_D = 100
-const KEY_Q = 113
-const KEY_F = 102
+const KEY_W = 87 // 119
+const KEY_A = 65 // 97
+const KEY_S = 83 // 115
+const KEY_D = 68 // 100
+const KEY_Q = 81 // 113
+const KEY_R = 82 // 114
+const KEY_F = 70 // 102
+
+GRAVITY = 6.67e-11 * 3e9 // Scale up by a billion or so just to help things out
 
 // Colours
 const BACKGROUND_COLOUR = 'rgb(237, 137, 17)'
@@ -25,14 +30,13 @@ const LIGHT_COLOUR = 'rgb(240, 240, 240)'
 const PLAYER_COLOUR = 'rgb(178, 28, 228)'
 const PLAYER_BORDER_COLOUR = 'rgb(128, 1, 178)'
 
-const ZERO_V = Victor(0, 0)
-
 // Global array
 let points = []
 
 // Some state
 let numRemaining = NUM_POINTS
 let timeElapsed = 0
+let timeElapsedMs = 0
 
 // Runs after page loads
 $(function() {
@@ -64,19 +68,20 @@ $(function() {
   function randomDirection() {
     const x = Math.random()
     const y = Math.random()
-    return Victor(x,y).normalize()
+    return new Two.Vector(x,y).normalize()
   }
   // avoids picking too close to the edge
   function randomLocation() {
     const x = randomInt(two.width - 100) + 50 // dont get to close to the edges
     const y = randomInt(two.height - 100) + 50
-    return new Victor(x, y)
+    return new Two.Vector(x, y)
   }
-
-  // Scale a unit vector (library didnt have this)
-  function scale(unitVector, scaleFactor) {
-    const scaling = new Victor(scaleFactor, scaleFactor)
-    return unitVector.multiply(scaling)
+  // Helper to write the time string to the screen
+  function getTimeString(time) {
+    const secs = time % 60
+    const mins = Math.floor(time / 60)
+    const secsString = secs >= 10 ? `${secs}` : `0${secs}`
+    return `Time Elapsed: ${mins}:${secsString}`
   }
 
   // Draws a new point and adds to array
@@ -90,7 +95,7 @@ $(function() {
     point.stroke = border
     point.linewidth = POINT_BORDER_THICKNESS
     // Save this points and info, into a list of shapes
-    points.push({id: id, shape: point, vel: vel, mass: mass})
+    points.push({id: id, shape: point, pos: pos, vel: vel, mass: mass, dead: 0})
   }
 
   // Draw all the points
@@ -104,11 +109,11 @@ $(function() {
     points = [points[0]] // just preserve player
 
     // Draw a bunch of random points, player is id 0
-    for (let id = 1; id < NUM_POINTS; id++) {
+    for (let id = 1; id <= NUM_POINTS; id++) {
       const pos = randomLocation();
-      const vel = scale(randomDirection(), 1)
       const radius = randomInt(MAX_RADIUS - MIN_RADIUS) + MIN_RADIUS
       const mass = getMass(radius)
+      const vel = new Two.Vector(0, 0)
       addPoint(id, radius, pos, vel, mass)
     }
   }
@@ -120,13 +125,15 @@ $(function() {
   background.noStroke().fill = BACKGROUND_COLOUR
 
   // Add a player point
-  const player = two.makeCircle(two.width/2, two.height/2, PLAYER_RADIUS)
+  const startingPoint = new Two.Vector(two.width/2, two.height/2)
+  const player = two.makeCircle(startingPoint.x, startingPoint.y, PLAYER_RADIUS)
   player.fill = PLAYER_COLOUR
   player.stroke = PLAYER_BORDER_COLOUR
-  points.push({id: 0, shape: player, vel: ZERO_V, mass: getMass(PLAYER_RADIUS)})
+  player.linewidth = POINT_BORDER_THICKNESS
+  points.push({id: 0, shape: player, pos: startingPoint, vel: new Two.Vector(0,0), mass: getMass(PLAYER_RADIUS)})
 
   const styles = {fill: PLAYER_COLOUR, size: 15, alignment: 'left'}
-  const infoText = two.makeText("Use WASD to control. F to brake. Q for help.", 20, 10, styles);
+  const infoText = two.makeText("Use WASD to control. F to brake. R to reset.", 20, 10, styles);
   const timeText = two.makeText("Time Elapsed: 0:00", 20, 30, styles);
   const remainingText = two.makeText("Points left: " + NUM_POINTS, 20, 50, styles);
 
@@ -135,30 +142,99 @@ $(function() {
   two.update();
 
   // Handle Keyboard presses
-  $(document).on('keypress', (e) => {
+  $(document).on('keydown', (e) => {
     // On WASD, change player velocity
       if(e.keyCode == KEY_W){
-        points[0].vel.y -= 0.2
+        points[0].vel.y -= KEY_STRENGTH
       }
       if(e.keyCode == KEY_A){
-        points[0].vel.x -= 0.2
+        points[0].vel.x -= KEY_STRENGTH
       }
-      if(e.keyCode == KEY_W){
-        points[0].vel.x += 0.2
+      if(e.keyCode == KEY_S){
+        points[0].vel.y += KEY_STRENGTH
       }
-      if(e.keyCode == KEY_W){
-        points[0].vel.y += 0.2
+      if(e.keyCode == KEY_D){
+        points[0].vel.x += KEY_STRENGTH
       }
       if(e.keyCode == KEY_F){
-        points[0].vel = scale(points[0].vel, 0.8)
+        points[0].vel = points[0].vel.multiplyScalar(0.8)
+      }
+      if(e.keyCode == KEY_R){
+        points[0].vel = new Two.Vector(0, 0)
+        points[0].pos = new Two.Vector(two.width/2, two.height/2)
       }
   });
-  function step() {
-    if(NUM_POINTS === 0) {
+
+  function isOffscreen(point) {
+    return (point.pos.x < -50
+      || point.pos.x > (two.width + 50)
+      || point.pos.y < -50
+      || point.pos.y > (two.height + 50))
+  }
+  // Here is the real physics!
+  function movePoints(dt) {
+    for(us of points){
+      // Don't simulate dead points
+      if(us.dead){
+        continue
+      }
+      // Set points as dead if they are offscreen and not us
+      if(isOffscreen(us) && us.id !== 0){
+        console.log('got here')
+        us.dead = 1
+        numRemaining -= 1
+      }
+      let netForce = new Two.Vector(0, 0) // start with empty force
+      for(them of points) {
+        // get contributions from all other shapes
+        if(us.id === them.id) {
+          continue // don't interact with ourself
+        }
+        // add force
+        // F = Gm1m2/r^2
+        const theirPos = them.pos.clone()
+        const ourPos = us.pos.clone()
+        const r = theirPos.subtract(ourPos) // vector subtraction
+        const distance = r.length() // magnitude
+        const forceMagnitude = (GRAVITY * us.mass * them.mass) / (distance**2)
+        r.normalize()
+        const force = r.multiplyScalar(forceMagnitude);
+        netForce.add(force) // add the scaled new force vector to the current force
+      }
+
+      // Fnet = ma => a = Fnet/m
+      const accel = netForce.divideScalar(us.mass)
+      // Update Velocity: deltav = a * deltat
+      us.vel.add(accel.multiplyScalar(dt))
+      // Update Position: distance travelled = v * deltat
+      us.pos.add(new Two.Vector(us.vel.x * dt, us.vel.y * dt))
+      us.shape.translation.set(us.pos.x, us.pos.y);
+
+    }
+  }
+  function step(frameCount, timeDelta) {
+    timeElapsedMs += timeDelta
+    movePoints(timeDelta/1000) // this needs to be in seconds
+    if(timeElapsedMs >= 1000) {
+      timeElapsedMs = 0
+      timeElapsed += 1
+      timeText.value = getTimeString(timeElapsed)
+    }
+    remainingText.value = "Points left: " + numRemaining
+
+    if(numRemaining === 0) {
       // Reset everything for the next trial!
-      console.log('Resetting!')
-      drawTshapes();
+      console.log('Resetting All!')
+      timeElapsed = 0
+      timeElapsedMs = 0
+      numRemaining = NUM_POINTS
+      points[0].vel = new Two.Vector(0, 0)
+      points[0].pos = new Two.Vector(two.width/2, two.height/2)
+      drawPoints();
       two.update();
     }
   }
+
+  two.bind('update', step).play();
+
 });
